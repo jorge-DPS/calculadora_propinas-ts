7bde2d1513c5fca0523580e64fba2582113ff42b
unknown <jorgedanielpacosillo@gmail.com>
2026-02-28 12:20:06 -0400

    iniciando con el proyecto con TS

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
+
+function App() {
+
+
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de propinas y consumo
+        </h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+          {menuItems.map((item) => (
+            <MenuItem
+              key={item.id}
+
+             />
+          ))}
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
